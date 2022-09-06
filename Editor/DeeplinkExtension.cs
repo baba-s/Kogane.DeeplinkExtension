728e3c1dee878986549b93da2d45f2b6d1745b94
baba-s <shotababa1206@gmail.com>
2022-09-06 19:44:41 +0900

    add

---
+++ Editor/DeeplinkExtension.cs
@@ -0,0 +1,50 @@
+﻿using JetBrains.Annotations;
+using Needle.Deeplink;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class DeeplinkExtension
+    {
+        [DeepLink( RegexFilter = @"com.unity3d.kharma:execute-menu-item\/(.*)" )]
+        [UsedImplicitly]
+        private static bool ExecuteMenuItem( string menuItemPath )
+        {
+            Debug.Log( menuItemPath );
+            return EditorApplication.ExecuteMenuItem( menuItemPath );
+        }
+
+        [DeepLink( RegexFilter = @"com.unity3d.kharma:ping-asset\/(.*)" )]
+        [UsedImplicitly]
+        private static bool PingAsset( string assetPath )
+        {
+            Debug.Log( assetPath );
+            var asset = AssetDatabase.LoadAssetAtPath<Object>( assetPath );
+            if ( asset == null ) return false;
+            EditorGUIUtility.PingObject( asset );
+            return true;
+        }
+
+        [DeepLink( RegexFilter = @"com.unity3d.kharma:ping-game-object\/(.*)" )]
+        [UsedImplicitly]
+        private static bool PingGameObject( string gameObjectName )
+        {
+            Debug.Log( gameObjectName );
+            var gameObject = GameObject.Find( gameObjectName );
+            if ( gameObject == null ) return false;
+            EditorGUIUtility.PingObject( gameObject );
+            return true;
+        }
+
+        [DeepLink( RegexFilter = @"com.unity3d.kharma:open-asset\/(.*)" )]
+        [UsedImplicitly]
+        private static bool OpenAsset( string assetName )
+        {
+            Debug.Log( assetName );
+            var asset = AssetDatabase.LoadAssetAtPath<Object>( assetName );
+            if ( asset == null ) return false;
+            return AssetDatabase.OpenAsset( asset );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Kogane.DeeplinkExtension
+# Kogane Deeplink Extension
+
+`com.needle.deeplink` の拡張
+
+## 使用例
+
+* [Profiler を開く](https://fwd.needle.tools/deeplink?com.unity3d.kharma:execute-menu-item/Window/Analysis/Profiler)
+* [SampleScene を Ping](https://fwd.needle.tools/deeplink?com.unity3d.kharma:ping-asset/Assets/Scenes/SampleScene.unity)
+* [Main Camera を Ping](https://fwd.needle.tools/deeplink?com.unity3d.kharma:ping-game-object/Main%20Camera)
+* [SampleScene を開く](https://fwd.needle.tools/deeplink?com.unity3d.kharma:open-asset/Assets/Scenes/SampleScene.unity)
