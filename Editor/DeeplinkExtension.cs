e9e7b5188ac8d474ef698f1349ca3d4a96eee1cc
baba-s <shotababa1206@gmail.com>
2022-09-06 19:57:04 +0900

    update

--- Editor/DeeplinkExtension.cs
+++ Editor/DeeplinkExtension.cs
@@ -1,7 +1,9 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 using Needle.Deeplink;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Kogane.Internal
 {
@@ -9,17 +11,19 @@ namespace Kogane.Internal
     {
         [DeepLink( RegexFilter = @"com.unity3d.kharma:execute-menu-item\/(.*)" )]
         [UsedImplicitly]
-        private static bool ExecuteMenuItem( string menuItemPath )
+        private static bool ExecuteMenuItem( string option )
         {
-            Debug.Log( menuItemPath );
-            return EditorApplication.ExecuteMenuItem( menuItemPath );
+            var menuItemName = Uri.UnescapeDataString( option );
+            Debug.Log( $"{nameof( ExecuteMenuItem )}: {menuItemName}" );
+            return EditorApplication.ExecuteMenuItem( menuItemName );
         }
 
         [DeepLink( RegexFilter = @"com.unity3d.kharma:ping-asset\/(.*)" )]
         [UsedImplicitly]
-        private static bool PingAsset( string assetPath )
+        private static bool PingAsset( string option )
         {
-            Debug.Log( assetPath );
+            var assetPath = Uri.UnescapeDataString( option );
+            Debug.Log( $"{nameof( PingAsset )}: {assetPath}" );
             var asset = AssetDatabase.LoadAssetAtPath<Object>( assetPath );
             if ( asset == null ) return false;
             EditorGUIUtility.PingObject( asset );
@@ -28,9 +32,10 @@ namespace Kogane.Internal
 
         [DeepLink( RegexFilter = @"com.unity3d.kharma:ping-game-object\/(.*)" )]
         [UsedImplicitly]
-        private static bool PingGameObject( string gameObjectName )
+        private static bool PingGameObject( string option )
         {
-            Debug.Log( gameObjectName );
+            var gameObjectName = Uri.UnescapeDataString( option );
+            Debug.Log( $"{nameof( PingGameObject )}: {gameObjectName}" );
             var gameObject = GameObject.Find( gameObjectName );
             if ( gameObject == null ) return false;
             EditorGUIUtility.PingObject( gameObject );
@@ -39,9 +44,10 @@ namespace Kogane.Internal
 
         [DeepLink( RegexFilter = @"com.unity3d.kharma:open-asset\/(.*)" )]
         [UsedImplicitly]
-        private static bool OpenAsset( string assetName )
+        private static bool OpenAsset( string option )
         {
-            Debug.Log( assetName );
+            var assetName = Uri.UnescapeDataString( option );
+            Debug.Log( $"{nameof( OpenAsset )}: {assetName}" );
             var asset = AssetDatabase.LoadAssetAtPath<Object>( assetName );
             if ( asset == null ) return false;
             return AssetDatabase.OpenAsset( asset );
